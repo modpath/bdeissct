01055a7db5dd3c27343b57e8bd5758a8d5650dba
azhukova <zhutchok@gmail.com>
2026-01-09 16:23:35 +0100

    Observed vs real R and d

--- simulations_bdeissct/py/observed_vs_real_values.py
+++ simulations_bdeissct/py/observed_vs_real_values.py
@@ -6,12 +6,9 @@ from treesimulator.mtbd_models import *
 
 model2n = {}
 for model_name in ('BD', 'BDEI', 'BDSS', 'BDEISS'): #, 'BDCT', 'BDEICT', 'BDSSCT', 'BDEISSCT'):
-    df = pd.DataFrame(columns=['type', 'tips',
+    df = pd.DataFrame(columns=['type',
                                'R',
-                               'd',
-                               'p',
-                               'z',
-                               'pi_E', 'pi_I', 'pi_S'])
+                               'd',])
 
     model_files = glob.glob(
         f'/home/azhukova/projects/bdeissct_dl/simulations_bdeissct/test/200_500/{model_name}/tree.*.log')
@@ -21,53 +18,21 @@ for model_name in ('BD', 'BDEI', 'BDSS', 'BDEISS'): #, 'BDCT', 'BDEICT', 'BDSSCT
         ddf = pd.read_csv(real)
         R = ddf.loc[0, 'R']
         d = ddf.loc[0, 'd']
-        p = ddf.loc[0, 'p_I']
-        z = ddf.loc[0, 'zeta']
-
-        pi_I_o = ddf.loc[0, 'pi_I_observed'] if 'pi_I_observed' in ddf.columns else 1
-        pi_I_C_o = ddf.loc[0, 'pi_I-C_observed'] if 'pi_I-C_observed' in ddf.columns else 0
-        pi_E_o = ddf.loc[0, 'pi_E_observed'] if 'pi_E_observed' in ddf.columns else 0
-        pi_E_C_o = ddf.loc[0, 'pi_E-C_observed'] if 'pi_E-C_observed' in ddf.columns else 0
-        pi_S_o = ddf.loc[0, 'pi_S_observed'] if 'pi_S_observed' in ddf.columns else 0
-        pi_S_C_o = ddf.loc[0, 'pi_S-C_observed'] if 'pi_S-C_observed' in ddf.columns else 0
-
-        if 'pi_I-C_observed' not in ddf.columns:
-            pi_I = ddf.loc[0, 'pi_I'] if 'pi_I' in ddf.columns else 1
-            pi_I_C = 0
-            pi_E = ddf.loc[0, 'pi_E'] if 'pi_E' in ddf.columns else 0
-            pi_E_C = 0
-            pi_S = ddf.loc[0, 'pi_S'] if 'pi_S' in ddf.columns else 0
-            pi_S_C = 0
-        else:
-            pi_I, pi_S, pi_E = pi_I_o, pi_S_o, pi_E_o
-            pi_I_C, pi_S_C, pi_E_C = pi_I_C_o, pi_S_C_o, pi_E_C_o
-
-        pi_I, pi_E, pi_S = pi_I / (pi_I + pi_E + pi_S), pi_E / (pi_I + pi_E + pi_S), pi_S / (pi_I + pi_E + pi_S)
-        pi_I_o, pi_E_o, pi_S_o = pi_I_o / (pi_I_o + pi_E_o + pi_S_o), pi_E_o / (pi_I_o + pi_E_o + pi_S_o), pi_S_o / (pi_I_o + pi_E_o + pi_S_o)
-
-        tips = ddf.loc[0, 'tips']
-
-        R_o = ddf.loc[0, 'avg_Re']
-        d_o = ddf.loc[0, 'avg_d']
-        z_o = ddf.loc[0, 'zeta']
+        R_o = ddf.loc[0, 'R_observed']
+        d_o = ddf.loc[0, 'd_observed']
 
 
 
         df.loc[f'{i}.real',
-        ['type', 'tips',
-         'R', 'd', 'p', 'z',
-         'pi_E', 'pi_I', 'pi_S']] \
-            = ['real', tips,
-               R, d, p, z,
-               pi_E, pi_I, pi_S]
+        ['type',
+         'R', 'd']] \
+            = ['real', R, d]
 
         df.loc[f'{i}.observed',
-        ['type', 'tips',
-         'R', 'd', 'p', 'z',
-         'pi_E', 'pi_I', 'pi_S']] \
-            = ['observed', tips,
-               R_o, d_o, z_o * R_o, z_o,
-               pi_E_o, pi_I_o, pi_S_o]
+        ['type',
+         'R', 'd']] \
+            = ['observed',
+               R_o, d_o]
 
     df.sort_index(inplace=True)
     df.index = df.index.map(lambda _: int(_.split('.')[0]))
@@ -78,20 +43,8 @@ for model_name in ('BD', 'BDEI', 'BDSS', 'BDEISS'): #, 'BDCT', 'BDEICT', 'BDSSCT
     df_obs = df[df['type'] == 'observed']
 
     R_relative_error = 100 * (df_obs['R'] - df_real['R']) / df_real['R']
-    print(f'R:\te={np.mean(np.abs(R_relative_error)):.0f},\tb={np.mean(R_relative_error):.0f}')
+    print(f'R:\te={np.mean(np.abs(R_relative_error)):.2f},\tb={np.mean(R_relative_error):.2f}')
     d_relative_error = 100 *(df_obs['d'] - df_real['d']) / df_real ['d']
-    print(f'd:\te={np.mean(np.abs(d_relative_error)):.0f},\tb={np.mean(d_relative_error):.0f}')
-    z_relative_error = 100 * (df_obs['z'] - df_real['z']) / df_real ['z']
-    print(f'z:\te={np.mean(np.abs(z_relative_error)):.0f},\tb={np.mean(z_relative_error):.0f}')
-    rho_relative_error = 100 * (df_obs['p'] - df_real['p']) / df_real ['p']
-    print(f'rho:\te={np.mean(np.abs(rho_relative_error)):.0f},\tb={np.mean(rho_relative_error):.0f}')
-    if 'EI' in model_name:
-        pi_E_relative_error = 100 * (df_obs['pi_E'] - df_real['pi_E']) / df_real ['pi_E']
-        print(f'pi_E:\te={np.mean(np.abs(pi_E_relative_error)):.0f},\tb={np.mean(pi_E_relative_error):.0f}')
-    if 'EI' in model_name or 'SS' in model_name:
-        pi_I_relative_error = 100 * (df_obs['pi_I'] - df_real['pi_I']) / df_real ['pi_I']
-        print(f'pi_I:\te={np.mean(np.abs(pi_I_relative_error)):.0f},\tb={np.mean(pi_I_relative_error):.0f}')
-    if 'SS' in model_name:
-        pi_S_relative_error = 100 * (df_obs['pi_S'] - df_real['pi_S']) / df_real ['pi_S']
-        print(f'pi_S:\te={np.mean(np.abs(pi_S_relative_error)):.0f},\tb={np.mean(pi_S_relative_error):.0f}')
+    print(f'd:\te={np.mean(np.abs(d_relative_error)):.2f},\tb={np.mean(d_relative_error):.2f}')
+
 

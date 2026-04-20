818bc42d2c4ae7cd42d896bddad3724bc3cb8e27
azhukova <zhutchok@gmail.com>
2026-04-20 22:33:58 +0200

    v0.1.96

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
         'Topic :: Software Development',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
-    version='0.1.95',
+    version='0.1.96',
     description='Estimation of BDEISS-CT parameters from phylogenetic trees.',
     author='Anna Zhukova',
     author_email='anna.zhukova@pasteur.fr',
